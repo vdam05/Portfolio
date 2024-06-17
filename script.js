8f55d3a27ee34385910a17ab704ed9b3367bbcb5
Viet Hoang Dam <vdam@uno.edu>
2024-06-17 19:14:33 +0700

    added special effect for name in js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let cons = "Welcome to JavaScript programming";
-console.log(cons);
+let colors = ["red", "blue", "green", "cyan", "blueviolet", "brown", "white"];
+let index = 0;
+let specialEffect = document.querySelector("#clickable");
+specialEffect.onclick = changeColors;
+
+function changeColors() {
+    index++;
+    if (index >= (colors.length)) {
+        index = 0;
+    } 
+    specialEffect.style.color = colors[index];
+    
+}
