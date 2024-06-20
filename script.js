faa8044bc148f29a9a6af7807a1d86848fa4f79c
Viet Hoang Dam <vdam@uno.edu>
2024-06-20 18:24:27 +0700

    started proj sect

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+//Variables
 let colors = ["red", "blue", "green", "cyan", "blueviolet", "brown"];
 let index = 0;
+
+/*---------------------------INTRO--------------------------------------*/ 
 let specialEffectName = document.querySelector("#clickable");
 specialEffectName.addEventListener('click', changeColors);
 function changeColors() {
@@ -10,3 +13,16 @@ function changeColors() {
     specialEffectName.style.color = colors[index];
     
 }
+/*---------------------------INTRO--------------------------------------*/ 
+
+/*---------------------------PROJECTS--------------------------------------*/ 
+let changeImage = document.querySelector(".projects-image");
+let option = document.querySelectorAll('.switching input[type="radio"]');
+option.addEventListener('click', switchImage);
+function switchImage(e) {
+    const currentImage = "";
+    currentImage += 
+    changeImage.style.backgroundImage = url(`./images/project-imgs/${current-image}.png`);
+}
+/*---------------------------PROJECTS--------------------------------------*/ 
+
