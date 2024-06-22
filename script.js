54578569fda34ff52a3743914726823b52995d90
Viet Hoang Dam <vdam@uno.edu>
2024-06-22 19:59:21 +0700

    did some adjusting

--- script.js
+++ script.js
@@ -13,17 +13,57 @@ function changeColors() {
     
 }
 //Changing the color of the header menu items
-let listItemSelect = document.querySelectorAll(".list-item");
+let listItemSelect = document.querySelectorAll(".list a");
 listItemSelect.forEach(item => item.addEventListener('click', menuSelected));
-function menuSelected (e) { //e for event
+function menuSelected (e) { 
+    for (const item of listItemSelect) {
+        item.style.color = "#e0e0e0";
+        item.style.textUnderlineOffset = "0%";
+        item.style.textDecoration = "none";
+    }
+    e.currentTarget.style.color = "black";
+    e.currentTarget.style.textDecoration = "underline";
+    e.currentTarget.style.textUnderlineOffset = "25%";
 }
 /*---------------------------INTRO--------------------------------------*/ 
 
 /*---------------------------SKILLS--------------------------------------*/ 
 
 /*---------------------------PROJECTS--------------------------------------*/ 
-let imageSelections = document.querySelectorAll("#imageId");
-imageSelections.forEach(selection => selection.addEventListener('checked'))
-
+//Switching project image and captions
+let captionArray = [
+    {
+        name: "image1",
+        caption: "This is my first Java project about making a basic game",
+    },
+    {
+        name: "image2",
+        caption: "Nothing here just yet",
+    },
+    {
+        name: "image3",
+        caption: "My website built with plain HTML, CSS, and JavaScript"
+    }
+];
+let projectButtons = document.querySelectorAll(".imageId");
+let projectImage = document.querySelector(".projects-image");
+let projectImageCaption = document.querySelector(".project-image-cap");
+projectButtons.forEach((button) => button.addEventListener("click", (e) => {
+    const chosenButton = e.currentTarget;
+    const chosenImg = `url(./images/project-imgs/${chosenButton.id}.png)`;
+    let chosenCaption = "";
+    projectImage.style.backgroundImage = chosenImg;
+    projectButtons.forEach((button) => {
+        button.style.backgroundColor = "transparent";
+    })
+    chosenButton.style.backgroundColor = "red";
+    projectImageCaption.innerText = "";
+    try {
+        chosenCaption = captionArray.find((element) => element.name === chosenButton.id).caption;
+    } catch (error) {
+        chosenCaption= "Seems like there is nothing here??? Check out my other things then.";
+    }
+    projectImageCaption.innerText = chosenCaption;
+}));
 /*---------------------------PROJECTS--------------------------------------*/ 
 
